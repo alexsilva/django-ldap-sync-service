fbaa75820a5e2ea988c7c5e94686c2cef9eae374
Alex Sandro <geniofuturo@gmail.com>
2016-10-27 18:04:28 -0200

    Adding installer with dependency settings.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='',
+    version='1.0',
+    py_modules=['ldap_sync_service'],
+    install_requires=['pyad'],
+    dependency_links=[
+        'git+https://github.com/alexsilva/django-ldap-sync.git@master'
+    ],
+    url='https://github.com/alexsilva/django-ldap-sync-service',
+    license='MIT',
+    author='alex',
+    author_email='',
+    description='Service used in django ldap sync command.',
+)
