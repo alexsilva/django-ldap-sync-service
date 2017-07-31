357609025f0774d087578e0c4c7f227e1b7ac610
alex <geniofuturo@gmail.com>
2017-07-31 13:37:51 -0300

    New implementation of the service to fetch user and groups.

--- ldap_sync_service.py
+++ ldap_sync_service.py
@@ -1,29 +1,59 @@
-from ldap_sync.service import Service
-from pyad import *
+from ldap3 import Server, Connection, ALL, NTLM
+
+from ldap_sync import service
 
 
-class PyadService(Service):
+class LdapSearchBindException(service.LdapSearchException):
+    """ Exception raised when bind does not occur as expected """
+
+
+class LdapSearchService(service.LdapSearch):
     """Implements the service interface"""
 
     def __init__(self, uri):
-        super(PyadService, self).__init__(uri)
-        pyad.set_defaults(ldap_server=uri)
+        super(LdapSearchService, self).__init__(uri)
 
-    def _search_users(self, base, filter, attributes):
+        self.server = Server(uri, get_info=ALL)
+        self._conn = self.username = self.password = None
+
+    @property
+    def connection(self):
+        if self._conn is None:
+            self._conn = Connection(self.server,
+                                    user=self.username,
+                                    password=self.password,
+                                    authentication=NTLM)
+        return self._conn
+
+    def users(self, sbase, sfilter, attributes):
         """Search for users"""
-        groups = adgroup.ADGroup.from_dn(filter + "," + base)
-        users = []
-        for user in groups.get_members():
-            users.append((user.cn, {k: (getattr(user, k),) for k in attributes}))
-        return users
+        items = []
+        if self.connection.search(search_base=sbase,
+                                  search_filter=sfilter,
+                                  attributes=attributes):
+            for entry in self.connection.entries:
+                attrs = {}
+                for attr in attributes:
+                    attrs[attr] = getattr(entry, attr)
+                data = {
+                    "attributes": attrs,
+                    "json": entry.entry_to_json()
+                }
+                items.append(data)
+        else:
+            raise service.LdapSearchException("empty search")
+        return items
 
-    def _search_groups(self, base, filter, attributes):
+    def groups(self, sbase, sfilter, attributes):
+        """Search for groups"""
         return []
-
-    def search(self, base, filter, attributes, objectype=None):
-        """generic search"""
-        return getattr(self, '_search_' + objectype)(base, filter, attributes)
 
     def login(self, username, password):
         """make login"""
-        pyad.set_defaults(username=username, password=password)
+        self.username = username
+        self.password = password
+
+        conn = self.connection
+
+        if not conn.bind():
+            raise LdapSearchBindException('error in bind {0.result}'.format(conn))
