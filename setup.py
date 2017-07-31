c4f35302aad90b5fa36742355104bbb5cf4f3626
alex <geniofuturo@gmail.com>
2017-07-31 18:26:40 -0300

    Change version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ldap-sync-service',
-    version='1.0',
+    version='2.0',
     py_modules=['ldap_sync_service'],
     install_requires=['ldap3'],
     dependency_links=[
